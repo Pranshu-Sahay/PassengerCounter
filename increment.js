386829c45948c784174ac3bd014a6c27e1d81994
Pranshu-Sahay <98802936+Pranshu-Sahay@users.noreply.github.com>
2023-05-20 02:01:20 +0530

    Day 1 final commit

--- increment.js
+++ increment.js
@@ -5,19 +5,40 @@
 // BonusPoints = BonusPoints + 70
 // console.log(BonusPoints)
 
+/*       */
 
-
-
-
-// Steps for increment button
+//  Steps for increment button
 // initialize count to 0
 // clicks in incrmt button
 // incrmt when button is clicked
 // change count-el in html to reflect new count
 
+/*       */
 
-function increment()
+// function increment()
+// {
+//     console.log("hello")
+// }
+
+/*       */
+
+// function Baba()
+// {
+//     console.log("Jai Ho")
+// }
+
+// Baba()/
+
+/*       */
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function total()
 {
-    console.log("hello")
+    let totalRaceTime = lap1+lap2+lap3
+    console.log(totalRaceTime)
 }
 
+total()
